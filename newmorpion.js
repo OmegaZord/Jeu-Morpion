30fcdcbb00defe7e412d7013dce2764e4d51971c
UTILISATEUR <UTILISATEUR@192.168.1.165>
2017-07-26 10:46:22 +0200

    IA et coloration
    Encore amélioration de l'IA, coloration de la ligne gagnante

--- newmorpion.js
+++ newmorpion.js
@@ -2,6 +2,7 @@
 			var pseudoJ1 = "X";
 			var pseudoJ2 = "O";
 			var iaPlayer = false;
+			var gameOver = false;
 			var tabCase = [
 				["case1", false, "a"], 
 				["case2", false, "b"], 
@@ -46,102 +47,134 @@
 			
 			function caseclick(caseid)
 			{
-				var numeroCase = parseInt(caseid.slice(4, 6)) -1;
-				
-				if(tabCase[numeroCase][1] == false)
-				{
-					tabCase[numeroCase][1]= true;
-					tabCase[numeroCase][2]= currentPlayer;
+				if(gameOver == false)
+				{			
+					var numeroCase = parseInt(caseid.slice(4, 6)) -1;
 					
-					if(currentPlayer == 1)
+					if(tabCase[numeroCase][1] == false)
 					{
-						document.getElementById(caseid).innerHTML = "X";
-						currentPlayer = 2;
+						tabCase[numeroCase][1]= true;
+						tabCase[numeroCase][2]= currentPlayer;
+						
+						if(currentPlayer == 1)
+						{
+							document.getElementById(caseid).innerHTML = "X";
+							currentPlayer = 2;
+						}
+						else if(currentPlayer == 2)
+						{
+							document.getElementById(caseid).innerHTML = "O";
+							currentPlayer = 1;
+						}
 					}
-					else if(currentPlayer == 2)
-					{
-						document.getElementById(caseid).innerHTML = "O";
-						currentPlayer = 1;
-					}
-				}
-				
-				var win = checkWinner();
-			
-				
-				if(win != null)
-				{
-					var node = document.createElement("LI");
-					var textnode = "";
 					
-					//win et pseudo inversé parceque la win à lieu dans le tour de l'autre joueur
-					if(win == 1)
-					{
-						textnode = document.createTextNode("Le gagnant est " + pseudoJ2);
-					}
-					else if(win == 2)
-					{
-						textnode = document.createTextNode("Le gagnant est " + pseudoJ1);
-					}
-					node.appendChild(textnode);
-					document.getElementById("victoires").appendChild(node);
-					
-					document.getElementById("nmbGame").innerHTML = parseInt(document.getElementById("nmbGame").innerHTML) + 1;
-					
-					reset();
-				}
+					var win = checkWinner();
 				
-				if(iaPlayer == true && currentPlayer == 2)
-				{
-					IAChoice();
+					
+					if(win != null)
+					{
+						gameOver = true;
+						
+						var node = document.createElement("LI");
+						var textnode = "";
+						
+						//win et pseudo inversé parceque la win à lieu dans le tour de l'autre joueur
+						if(win == 1)
+						{
+							textnode = document.createTextNode("Le gagnant est " + pseudoJ2);
+							node.appendChild(textnode);
+							node.classList.add("list-group-item");
+							node.classList.add("list-group-item-danger");
+						}
+						else if(win == 2)
+						{
+							textnode = document.createTextNode("Le gagnant est " + pseudoJ1);
+							node.appendChild(textnode);
+							node.classList.add("list-group-item");
+							node.classList.add("list-group-item-success");
+						}
+					
+						document.getElementById("victoires").appendChild(node);
+						
+						document.getElementById("nmbGame").innerHTML = parseInt(document.getElementById("nmbGame").innerHTML) + 1;
+						
+						//reset();
+					}
+					
+					if(gameOver == false && win == null && iaPlayer == true && currentPlayer == 2)
+					{
+						IAChoice();
+					}
 				}
 			}
 			
 			function checkWinner()
 			{
-				
 				//Vérifications horizontales
 				if(tabCase[0][2] == tabCase[1][2]  && tabCase[1][2] == tabCase[2][2])
 				{
 					console.log("1");
+					
+					colorWinGrid(0, 1, 2);
+						
 					return currentPlayer;
 				}
 				if(tabCase[3][2] == tabCase[4][2]  && tabCase[4][2] == tabCase[5][2])
 				{
 					console.log("2");
+					
+					colorWinGrid(3, 4, 5);
+						
 					return currentPlayer;
 				}
 				if(tabCase[6][2] == tabCase[7][2] && tabCase[7][2] == tabCase[8][2])
 				{
 					console.log("3");
+					
+					colorWinGrid(6, 7, 8);
+						
 					return currentPlayer;
 				}
 				//Vérifications verticales
 				if(tabCase[0][2] == tabCase[3][2] && tabCase[3][2] == tabCase[6][2])
 				{
-					console.log(tabCase[0][2] == tabCase[3][2]);
-					console.log(tabCase[3][2] == tabCase[6][2]);
 					console.log("4");
+					
+					colorWinGrid(0, 3, 6);
+						
 					return currentPlayer;
 				}
 				if(tabCase[1][2] == tabCase[4][2]  && tabCase[4][2] == tabCase[7][2])
 				{
 					console.log("5");
+					
+					colorWinGrid(1, 4, 7);
+						
 					return currentPlayer;
 				}
 				if(tabCase[2][2] == tabCase[5][2] && tabCase[5][2] == tabCase[8][2])
 				{
 					console.log("6");
+					
+					colorWinGrid(2, 5, 8);
+						
 					return currentPlayer;
 				}
 				//Vérifications diagonales
 				if(tabCase[0][2] == tabCase[4][2] && tabCase[4][2] == tabCase[8][2])
 				{
 					console.log("7");
+					
+					colorWinGrid(0, 4, 8);
+						
 					return currentPlayer;
 				}
 				if(tabCase[2][2] == tabCase[4][2] && tabCase[4][2] == tabCase[6][2])
 				{
 					console.log("8");
+					
+					colorWinGrid(2, 4, 6);
+						
 					return currentPlayer;
 				}
 				return null;
@@ -150,11 +183,11 @@
 			function reset()
 			{
 				//Vider la grille
-				var str = "";
+				var casesGrille = document.getElementsByClassName("caseMorpion");
+				
 				for(var i =0; i<9; i++)
 				{
-					str = tabCase[i][0];
-					document.getElementById(str).innerHTML = "";
+					casesGrille[i].innerHTML = "";
 				}
 				
 				//Reinitialiser le tableau
@@ -178,6 +211,8 @@
 				selector.value = "styleDefault";
 				
 				changeStyle();
+				
+				gameOver = false;
 			}
 			
 			function changeStyle()
@@ -241,24 +276,28 @@
 				}
 			}
 			
+			function colorWinGrid(case1, case2, case3)
+			{
+				var casesGrille = document.getElementsByClassName("caseMorpion");
+				
+				casesGrille[case1].style.background = "Salmon";
+				casesGrille[case2].style.background = "Salmon";
+				casesGrille[case3].style.background = "Salmon";
+			}
+
 			function IAChoice()
 			{
 				var tabCaseEmpty = [];
 				
 				for(var i =0; i<9; i++)
 				{
-					
 					if(tabCase[i][1] == false)
 					{
 						tabCaseEmpty.push(tabCase[i]);
 					}
 				}
 				
-				//console.log(tabCaseEmpty);
-				
 				var testIA = tabCaseEmpty[Math.floor(Math.random() * tabCaseEmpty.length)];
 				
-				//console.log(testIA);
-				//console.log(testIA[0]);
 				caseclick(testIA[0]);
 			}
